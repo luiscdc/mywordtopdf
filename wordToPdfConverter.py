66739cdb14836863b46a69e200ea86b7f5ae36f3
luiscdc <68789989+luiscdc@users.noreply.github.com>
2020-08-01 22:20:12 +0200

    Add files via upload
    Description of the steps.

---
+++ wordToPdfConverter.py
@@ -0,0 +1,9 @@
+#Install package
+
+#Import library
+
+#Ask for the name of the file. The file should be placed in the desktop.
+
+# Do the processing
+
+# Give the user the following message: "Your .doc file was converted into .pdf and named as XXX. It is saved on the desktop."
